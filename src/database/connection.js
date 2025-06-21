36795ee8c919430fef51d5bc6ed582bf7d142844
kiskey <59226371+kiskey@users.noreply.github.com>
2025-06-20 21:17:24 -0700

    Update connection.js

--- src/database/connection.js
+++ src/database/connection.js
@@ -5,22 +5,26 @@ const defineModels = require('./models');
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: './stremio_addon.db',
+    // FIX: Use the absolute path to the data volume we mounted in Docker.
+    // This ensures the database file is written to the persistent storage location.
+    storage: '/data/stremio_addon.db',
     logging: msg => logger.debug(msg),
 });
 
 const models = defineModels(sequelize);
 
-// Define associations
-models.Thread.belongsTo(models.TmdbMetadata, { foreignKey: 'tmdb_id', targetKey: 'tmdb_id' });
-models.TmdbMetadata.hasMany(models.Thread, { foreignKey: 'tmdb_id', sourceKey: 'tmdb_id' });
+// Define associations between models
+if (models.Thread && models.TmdbMetadata) {
+    models.Thread.belongsTo(models.TmdbMetadata, { foreignKey: 'tmdb_id', targetKey: 'tmdb_id' });
+    models.TmdbMetadata.hasMany(models.Thread, { foreignKey: 'tmdb_id', sourceKey: 'tmdb_id' });
+}
 
 const syncDb = async () => {
     try {
-        await sequelize.sync({ alter: true }); // 'alter: true' avoids data loss on schema changes
+        await sequelize.sync({ alter: true });
         logger.info('Database & tables created!');
     } catch (error) {
-        logger.error('Error synchronizing database:', error);
+        logger.error(error, 'Error synchronizing database:');
     }
 };
 
