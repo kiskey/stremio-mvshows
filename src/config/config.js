de604c374f84c92883cb8a0f6dd289bca4a7c11f
kiskey <59226371+kiskey@users.noreply.github.com>
2025-06-22 00:12:45 -0700

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -21,7 +21,7 @@ const config = {
     addonDescription: 'A Stremio addon providing streams from a TamilMV torrent forum.',
     addonVersion: '1.0.0',
        // NEW: A placeholder poster for content that hasn't been matched yet.
-    placeholderPoster: 'https://i.imgur.com/b54abw2.png',
+    placeholderPoster: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/da/Aha_%28streaming_service.svg/250px-Aha_%28streaming_service.svg.png',
 
     trackers: [
         "udp://tracker.opentrackr.org:1337/announce",
